dbe6445fa59ca17ef2cd3378848cd7593c6913f4
Vui Nguyen <vui_engineer@yahoo.com>
2019-03-15 09:26:51 -0600

    Initial Commit

---
+++ ShibbolethAuthTests/ShibbolethAuthTests.swift
@@ -0,0 +1,34 @@
+//
+//  ShibbolethAuthTests.swift
+//  ShibbolethAuthTests
+//
+//  Created by Vui Nguyen on 3/15/19.
+//  Copyright © 2019 Minitex. All rights reserved.
+//
+
+import XCTest
+@testable import ShibbolethAuth
+
+class ShibbolethAuthTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
